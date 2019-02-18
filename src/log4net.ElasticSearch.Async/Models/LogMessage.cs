54a89a5a4de4add2d4dfa716807718c52f7eee16
Cleyton Souza <c.souza@centauro.com>
2019-02-18 19:07:46 -0300

    Updating Framework version to 4.7.2. Removing some projects.

---
+++ src/log4net.ElasticSearch.Async/Models/LogMessage.cs
@@ -0,0 +1,10 @@
+﻿
+namespace log4net.ElasticSearch.Async.Models
+{
+    public class LogMessage
+    {
+        public string  Message {get;set;}
+        public object Object { get; set; }
+
+    }
+}
